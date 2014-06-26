ec4de3b6ec5ae4e49b1220cce9711bdb3465bc30
Róbert Selvek <robert.selvek@gmail.com>
2014-06-26 20:45:51 +0200

    Add setImmediate into example

--- example/walk.js
+++ example/walk.js
@@ -1,8 +1,10 @@
 var walker = require('../lib/walker.js');
-
+var timers = require("timers");
 walker.walk(process.argv[2], process.argv[3], function(location, done) {
     console.log('do stuff %s', JSON.stringify(location));
-    done();
+    //You should use timers.setImmediate because otherwise,
+    //the stack traces can get extremely long.
+    timers.setImmediate(done());
 }, function(e) {
     if(e) {
         console.log(e);
